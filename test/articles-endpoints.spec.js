ce7bd4864a14a3146d0312e2dd6c612327039f45
yl1462 <yliang1462@hotmail.com>
2021-01-27 14:39:54 -0600

    checkpoint 15 lesson

--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -1,8 +1,3 @@
-const { expect } = require('chai')
-const knex = require('knex')
-const app = require('../src/app')
-const { makeArticlesArray } = require('./articles.fixtures')
-
 describe('Articles Endpoints', function() {
   let db
 
@@ -20,27 +15,58 @@ describe('Articles Endpoints', function() {
 
   afterEach('cleanup',() => db('blogful_articles').truncate())
 
-  context('Given there are articles in the database', () => {
-    const testArticles = makeArticlesArray()
-  
-    beforeEach('insert articles', () => {
-      return db
-        .into('blogful_articles')
-        .insert(testArticles)
+  describe(`GET /articles`, () => {
+    context(`Given no articles`, () => {
+      it(`responds with 200 and an empty list`, () => {
+        return supertest(app)
+          .get('/articles')
+          .expect(200, [])
+      })
     })
 
-    it('GET /articles responds with 200 and all of the articles', () => {
-      return supertest(app)
-        .get('/articles')
-        .expect(200, testArticles)
+    context('Given there are articles in the database', () => {
+      const testArticles = makeArticlesArray()
+
+      beforeEach('insert articles', () => {
+        return db
+          .into('blogful_articles')
+          .insert(testArticles)
+      })
+
+      it('responds with 200 and all of the articles', () => {
+        return supertest(app)
+          .get('/articles')
+          .expect(200, testArticles)
+      })
+    })
+  })
+
+  describe(`GET /articles/:article_id`, () => {
+    context(`Given no articles`, () => {
+      it(`responds with 404`, () => {
+        const articleId = 123456
+        return supertest(app)
+          .get(`/articles/${articleId}`)
+          .expect(404, { error: { message: `Article doesn't exist` } })
+      })
     })
 
-    it('GET /articles/:article_id responds with 200 and the specified article', () => {
-      const articleId = 2
-      const expectedArticle = testArticles[articleId - 1]
-      return supertest(app)
-        .get(`/articles/${articleId}`)
-        .expect(200, expectedArticle)
+    context('Given there are articles in the database', () => {
+      const testArticles = makeArticlesArray()
+
+      beforeEach('insert articles', () => {
+        return db
+          .into('blogful_articles')
+          .insert(testArticles)
+      })
+
+      it('responds with 200 and the specified article', () => {
+        const articleId = 2
+        const expectedArticle = testArticles[articleId - 1]
+        return supertest(app)
+          .get(`/articles/${articleId}`)
+          .expect(200, expectedArticle)
+      })
     })
   })
 })
